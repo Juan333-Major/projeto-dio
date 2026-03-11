4da470ee2a4784d9404f78a127d701e89194edb2
Juan333-Major <juancrls.894@gmail.com>
2026-03-10 22:33:34 -0300

    codigo foi concluido

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-let nome = "Juan"
-console.log(nome)
+const nome = "Juan"
+let nivel = ""
+let xp = 1000
+
+if(xp <= 1000){
+    nivel = "Ferro"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+}else if(xp >= 1001 || xp <= 5000){
+    nivel = "Prata"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+}else if(xp >= 5001 || xp <= 7000){
+    nivel = "Ouro"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+}else if(xp >= 7001 || xp <= 8000){
+    nivel = "Platina"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+}else if(xp >= 8001 || xp <= 9000){
+    nivel = "Ascendente"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+}else if(xp >= 9001 || xp <= 10000){
+    nivel = "Imortal"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+}else{
+    nivel = "Radiante"
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+}
